4ad475af3f5a14b846628d14d9aa2e23d245c96c
Ivan Kondzerau <konderev@aheadworks.com>
2025-04-19 19:07:19 +0300

    feat: add route - users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authRouter } from './interface/routes/auth.ts';
 import { postsRouter } from './interface/routes/posts.ts';
+import { usersRouter } from './interface/routes/users.ts';
 
 const PORT = 3000;
 const BASE_URL = '/api/v1';
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(BASE_URL, authRouter);
 app.use(BASE_URL, postsRouter);
+app.use(BASE_URL, usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
