695c4b24456d9092af091ff8d79a75e91f3cf67f
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2025-04-25 16:11:11 +0300

    fix: add commentId

--- src/domain/relationships/Like.ts
+++ src/domain/relationships/Like.ts
@@ -1,8 +1,9 @@
-import { UserId, PostId } from "../types/Identifiers";
+import { UserId, PostId, CommentId } from "../types/Identifiers";
 
 export class Like {
   constructor(
     public readonly userId: UserId,
-    public readonly postId: PostId
+    public readonly postId: PostId,
+    public readonly commentId: CommentId
   ) {}
 }
--- src/domain/types/Identifiers.ts
+++ src/domain/types/Identifiers.ts
@@ -2,3 +2,4 @@ type Brand<K,T> = K & {__brand: T};
 
 export type UserId = Brand<string, 'UserId'>;
 export type PostId = Brand<string, 'PostId'>;
+export type CommentId = Brand<string, 'CommentId'>;
