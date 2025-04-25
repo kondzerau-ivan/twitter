fcda87407422a1cd2624c9350075308b31a42e57
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2025-04-25 15:49:49 +0300

    fix: add generic Brand

--- src/domain/types/Identifiers.ts
+++ src/domain/types/Identifiers.ts
@@ -1,2 +1,4 @@
-export type UserId = string;
-export type PostId = string;
+type Brand<K,T> = K & {__brand: T};
+
+export type UserId = Brand<string, 'UserId'>;
+export type PostId = Brand<string, 'PostId'>;
