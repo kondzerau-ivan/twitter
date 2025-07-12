ad3eb5dbd68190493bef0e6854557d6f25cd08fe
Ivan Kondzerau <konderev@aheadworks.com>
2025-07-12 18:51:18 +0300

    fix: import/re-export models

--- src/domain/models/associations.ts
+++ src/domain/models/associations.ts
@@ -5,92 +5,89 @@ import { Comment } from "./Comment";
 import { Follow } from "./Follow";
 import { Like } from "./Like";
 
-export function defineAssociations() {
+User.hasOne(Auth, {
+  foreignKey: "userId",
+  as: "auth"
+});
 
-  User.hasOne(Auth, {
-    foreignKey: "userId",
-    as: "auth"
-  });
+Auth.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user"
+});
 
-  Auth.belongsTo(User, {
-    foreignKey: "userId",
-    as: "user"
-  });
+User.hasMany(Post, {
+  foreignKey: "authorId",
+  as: "posts"
+});
 
-  User.hasMany(Post, {
-    foreignKey: "authorId",
-    as: "posts"
-  });
+Post.belongsTo(User, {
+  foreignKey: "authorId",
+  as: "author"
+});
 
-  Post.belongsTo(User, {
-    foreignKey: "authorId",
-    as: "author"
-  });
+User.hasMany(Comment, {
+  foreignKey: "authorId",
+  as: "comments"
+});
 
-  User.hasMany(Comment, {
-    foreignKey: "authorId",
-    as: "comments"
-  });
+Comment.belongsTo(User, {
+  foreignKey: "authorId",
+  as: "author"
+});
 
-  Comment.belongsTo(User, {
-    foreignKey: "authorId",
-    as: "author"
-  });
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  as: "comments"
+});
 
-  Post.hasMany(Comment, {
-    foreignKey: "postId",
-    as: "comments"
-  });
+Comment.belongsTo(Post, {
+  foreignKey: "postId",
+  as: "post"
+});
 
-  Comment.belongsTo(Post, {
-    foreignKey: "postId",
-    as: "post"
-  });
+User.belongsToMany(User, {
+  through: Follow,
+  as: "followers", // подписчики
+  foreignKey: "followedId", // на кого подписались
+  otherKey: "followerId" // кто подписался
+});
 
-  User.belongsToMany(User, {
-    through: Follow,
-    as: "followers", // подписчики
-    foreignKey: "followedId", // на кого подписались
-    otherKey: "followerId" // кто подписался
-  });
+User.belongsToMany(User, {
+  through: Follow,
+  as: "followings", // подписки
+  foreignKey: "followerId", // кто подписан
+  otherKey: "followingId"   // на кого подписан
+});
 
-  User.belongsToMany(User, {
-    through: Follow,
-    as: "followings", // подписки
-    foreignKey: "followerId", // кто подписан
-    otherKey: "followingId"   // на кого подписан
-  });
+User.hasMany(Like, {
+  foreignKey: 'userId',
+  as: 'likes',
+});
 
-  User.hasMany(Like, {
-    foreignKey: 'userId',
-    as: 'likes',
-  });
+Like.belongsTo(User, {
+  foreignKey: 'userId',
+  as: 'user',
+});
 
-  Like.belongsTo(User, {
-    foreignKey: 'userId',
-    as: 'user',
-  });
+Post.hasMany(Like, {
+  foreignKey: 'targetId',
+  constraints: false,
+  scope: { targetType: 'post' },
+});
 
-  Post.hasMany(Like, {
-    foreignKey: 'targetId',
-    constraints: false,
-    scope: { targetType: 'post' },
-  });
+Like.belongsTo(Post, {
+  foreignKey: 'targetId',
+  constraints: false,
+});
 
-  Like.belongsTo(Post, {
-    foreignKey: 'targetId',
-    constraints: false,
-  });
+Comment.hasMany(Like, {
+  foreignKey: 'targetId',
+  constraints: false,
+  scope: { targetType: 'comment' },
+});
 
-  Comment.hasMany(Like, {
-    foreignKey: 'targetId',
-    constraints: false,
-    scope: { targetType: 'comment' },
-  });
-
-  Like.belongsTo(Comment, {
-    foreignKey: 'targetId',
-    constraints: false,
-  });
-}
+Like.belongsTo(Comment, {
+  foreignKey: 'targetId',
+  constraints: false,
+});
 
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,11 +1,8 @@
-import { Auth } from "./Auth";
-import { User } from "./User";
-import { Post } from "./Post";
-import { Comment } from "./Comment";
-import { Follow } from "./Follow";
-import { Like } from "./Like";
-import { defineAssociations } from "./associations";
+import "./associations";
 
-defineAssociations();
-
-export const models = { Auth, User, Post, Comment, Like, Follow };
+export * from "./Auth";
+export * from "./User";
+export * from "./Post";
+export * from "./Comment";
+export * from "./Follow";
+export * from "./Like";
